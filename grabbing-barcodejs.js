9d283e18a0ad1108789a910f3cf531c99fbaa506
Caplini <kylenoodles777@Gmail.com>
2023-07-02 12:42:42 +0100

    testing

--- grabbing-barcodejs.js
+++
@@ -1,9 +0,0 @@
-let totals = 0
-
-function addtotals() {
-    let toadd = document.getElementById("count").value;
-    let name = document.getElementById("itemname").value;
-
-    totals += eval(toadd);
-    document.getElementById("total").innerHTML = `${name}: ${totals}`;
-}
